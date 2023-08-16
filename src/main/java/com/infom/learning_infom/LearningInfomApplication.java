ff2ce005fc83dd5f4c472aeb961a958d72912fc1
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-08-17 04:06:00 +0800

    Display "Hello world!"

--- src/main/java/com/infom/learning_infom/LearningInfomApplication.java
+++ src/main/java/com/infom/learning_infom/LearningInfomApplication.java
@@ -2,13 +2,21 @@ package com.infom.learning_infom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LearningInfomApplication {
 
 	public static void main(String[] args) {
 		// Hello!
 		SpringApplication.run(LearningInfomApplication.class, args);
+	}
+
+	@GetMapping
+	public String greet() {
+		return "Hello world!";
 	}
 
 }
