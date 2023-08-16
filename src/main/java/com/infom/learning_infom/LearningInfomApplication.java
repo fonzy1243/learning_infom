dccf06250c891de1415003c618200d59244dcbc1
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-08-17 03:46:00 +0800

    Initial files

---
+++ src/main/java/com/infom/learning_infom/LearningInfomApplication.java
@@ -0,0 +1,13 @@
+package com.infom.learning_infom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningInfomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningInfomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infom/learning_infom/LearningInfomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infom.learning_infom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningInfomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
