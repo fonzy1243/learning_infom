13a2661de8f6fd7a530b91071b746efb5ddb4527
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-08-21 20:16:46 +0800

    Added react project

---
+++ src/main/java/com/infom/learning_infom/HomeownerController.java
@@ -0,0 +1,11 @@
+package com.infom.learning_infom;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class HomeownerController {
+
+}
